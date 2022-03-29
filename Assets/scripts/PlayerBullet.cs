b334b10d24290c01656ed9bbe3d2b2287f67a89a
Renzo <reemer2002@gmail.com>
2022-03-29 15:16:43 +0200

    made bullet clone en schiet vooruit

--- Assets/scripts/PlayerBullet.cs
+++ Assets/scripts/PlayerBullet.cs
@@ -2,23 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerBullet : MonoBehaviour
 {
+    public GameObject bullet;
     public Rigidbody rb;
     public static int movespeed = 10;
     public static int backwards = -10;
     public Vector3 userDirection = Vector3.right;
-    public bool canJump = false;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(bullet, new Vector3(1, 1, 1), Quaternion.identity);
+        }
+
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(new Vector3(0, -1, 0));
@@ -35,19 +40,6 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.Translate(userDirection * backwards * Time.deltaTime);
         }
-        if (canJump == true && Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity = new Vector3(rb.velocity.x, 10, 0);
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        canJump = true;
-    }
-
-    private void OnCollisionExit(Collision collision)
-    {
-        canJump = false;
+        
     }
 }
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public static int movespeed = 10;
+    public static int backwards = -10;
+    public Vector3 userDirection = Vector3.right;
+    public bool canJump = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(new Vector3(0, -1, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(new Vector3(0, 1, 0));
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(userDirection * movespeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(userDirection * backwards * Time.deltaTime);
+        }
+        if (canJump == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, 10, 0);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        canJump = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        canJump = false;
+    }
+}
