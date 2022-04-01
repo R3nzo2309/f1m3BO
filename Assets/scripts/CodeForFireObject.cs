6f83659904db21a9ae50348142139cefe57dfee1
Renzo <reemer2002@gmail.com>
2022-04-01 12:54:05 +0200

    nieuw

--- Assets/scripts/CodeForFireObject.cs
+++ Assets/scripts/CodeForFireObject.cs
@@ -27,11 +27,11 @@ public class CodeForFireObject : MonoBehaviour
 
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(new Vector3(0, -1, 0));
+            transform.Rotate(new Vector3(0, -0.1f, 0));
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(new Vector3(0, 1, 0));
+            transform.Rotate(new Vector3(0, 0.1f, 0));
         }
         if (Input.GetKey(KeyCode.W))
         {
